b481113e7ca484be11efd692e8c108b894b52e41
Tyler <devers.tyler@gmail.com>
2021-10-30 11:01:31 -0700

    first commit post clone

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.InputSystem; // from package manager, code for moving player
+    // uses Player Input component with an attached InputActions asset
+
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX, movementY;
+    public float speed = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Use input system package to move character
+    void OnMove(InputValue moveValue) {
+        Vector2 movementVector = moveValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void FixedUpdate() {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+}
